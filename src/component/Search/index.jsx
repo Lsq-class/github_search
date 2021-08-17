57da3b54bd0201c0ed3417b31228c7995349fcc5
lsq <359549701@qq.com>
2021-08-17 19:27:52 +0800

    search firstcommit

--- src/component/Search/index.jsx
+++ src/component/Search/index.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 
 export default class Search extends Component {
+
+  search=()=>{
+    console.log('我查到了')
+  }
     render() {
         return (
             <section class="jumbotron">
             <h3 class="jumbotron-heading">Search Github Users</h3>
             <div>
-              <input type="text" placeholder="enter the name you search"/>&nbsp;<button>Search</button>
+              <input type="text" placeholder="enter the name you search"/>&nbsp;<button onClick={this.search}>Search</button>
             </div>
           </section>
         )
