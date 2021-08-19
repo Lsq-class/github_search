49fd27419d364dab5753c8f447c5cd7b22c266fc
lsq <359549701@qq.com>
2021-08-19 18:19:12 +0800

    antd组件按需导入和主题色自定义

---
+++ craco.config.js
@@ -0,0 +1,25 @@
+const CracoLessPlugin = require('craco-less');
+const { override, fixBabelImports } = require('customize-cra');
+
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true,
+  }),
+);
+module.exports = {
+    plugins: [
+      {
+        plugin: CracoLessPlugin,
+        options: {
+          lessLoaderOptions: {
+            lessOptions: {
+              modifyVars: { '@primary-color': '#1DA57A' },
+              javascriptEnabled: true,
+            },
+          },
+        },
+      },
+    ],
+  };
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import Search from './component/Search'
 import List from './component/List'
+import { Button } from 'antd'
+import { DatePicker } from 'antd';
+
+const { RangePicker } = DatePicker;
 
 export default class App extends Component {
 
@@ -9,6 +13,10 @@ export default class App extends Component {
             <div className="container">
                 <Search/>
                 <List/>
+                <Button type="primary" size='large'  >
+          Primary
+        </Button>
+        <RangePicker />
             </div>
         )
     }
